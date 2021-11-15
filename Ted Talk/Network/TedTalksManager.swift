e402a0d81ac02814871760e4dd09c55c3efce071
Ignacio Cervino <ignacio.cervino@globant.com>
2021-11-15 17:40:00 -0300

    Creating a new viewcotroller with a scrollview to show the detail of a specific tedtalk the user selected in the main view

---
+++ Ted Talk/Network/TedTalksManager.swift
@@ -0,0 +1,41 @@
+//
+//  TedTalksManager.swift
+//  Ted Talk
+//
+//  Created by Ignacio Cervino on 16/11/2021.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case fileNotFound
+    case invalidData
+    case decodingProblem(String)
+}
+
+class TedTalksManager {
+    func retrieve(onCompletion: @escaping (Result<[TedTalk], Errors>) -> Void) {
+        
+        DispatchQueue.global(qos: .background).async {
+            let url = Bundle.main.url(forResource: "tedTalks", withExtension: "json")
+            guard let myUrl = url else {
+                onCompletion(.failure(.fileNotFound))
+                return
+            }
+            guard let myData = try? Data(contentsOf: myUrl) else {
+                onCompletion(.failure(.invalidData))
+                return
+            }
+            do {
+                let tedtalks = try JSONDecoder().decode([TedTalk].self, from: myData)
+                onCompletion(.success(tedtalks))
+            } catch DecodingError.dataCorrupted(_) {
+                onCompletion(.failure(.decodingProblem("Data corrupted")))
+            } catch DecodingError.keyNotFound(let codingKey, _) {
+                onCompletion(.failure(.decodingProblem(codingKey.stringValue)))
+            } catch let error {
+                onCompletion(.failure(.decodingProblem(error.localizedDescription)))
+            }
+        }
+    }
+}
