02a305061fd43f298098d34d30f38f947ae459e5
Ignacio Cervino <ignacio.cervino@globant.com>
2021-11-18 14:15:19 -0300

    Config swift lint in the project
    filtering ted talks by year of publication
    
    fixing gitignore

--- Ted Talk/Controller/TedTalksViewController.swift
+++ Ted Talk/Controller/TedTalksViewController.swift
@@ -7,86 +7,113 @@
 
 import UIKit
 
-class TedTalksViewController: UIViewController {
-    
-    @IBOutlet weak var tedTalksTableView: UITableView!
-    @IBOutlet weak var searchField: UITextField!
-    @IBOutlet weak var errorLabel: UILabel!
-    @IBOutlet weak var tedTalksActivityIndicator: UIActivityIndicatorView!
-    
-    var tedtalks: [TedTalk]? = [] {
-        didSet {
-            self.tedTalksTableView.reloadData()
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tedTalksActivityIndicator.startAnimating()
-        TedTalksManager().retrieve() {
-            [weak self] result in
-            DispatchQueue.main.async {
-                self?.tedTalksActivityIndicator.stopAnimating()
-                switch result {
-                case .success(let tedtalks):
-                    self?.tedtalks = tedtalks
-                    self?.errorLabel.isHidden = true
-                case .failure(let error):
-                    switch error {
-                    case .fileNotFound:
-                        self?.errorLabel.text = "File not found"
-                    case .decodingProblem(let problem):
-                        self?.errorLabel.text = "There was a problem decoding the data: \(problem)"
-                    default:
-                        self?.errorLabel.text = "There was a problem"
-                    }
-                    self?.errorLabel.isHidden = false
-                    self?.tedtalks = []
-                }
-            }
-        }
-        
-        tedTalksTableView.dataSource = self
-        tedTalksTableView.delegate = self
-    }
+class TedTalksViewController: UIViewController, UITextFieldDelegate {
+
+	@IBOutlet weak var tedTalksTableView: UITableView!
+	@IBOutlet weak var searchField: UITextField!
+	@IBOutlet weak var errorLabel: UILabel!
+	@IBOutlet weak var tedTalksActivityIndicator: UIActivityIndicatorView!
+
+	var tedtalks: [TedTalk]? = [] {
+		didSet {
+			self.tedTalksTableView.reloadData()
+		}
+	}
+
+	var tedtalksFiltered: [TedTalk]? = []
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tedTalksActivityIndicator.startAnimating()
+		TedTalksManager().retrieve { [weak self] result in
+			DispatchQueue.main.async {
+				self?.tedTalksActivityIndicator.stopAnimating()
+				switch result {
+				case .success(let tedtalks):
+					self?.tedtalks = tedtalks
+					self?.errorLabel.isHidden = true
+				case .failure(let error):
+					switch error {
+					case .fileNotFound:
+						self?.errorLabel.text = "File not found"
+					case .decodingProblem(let problem):
+						self?.errorLabel.text = "There was a problem decoding the data: \(problem)"
+					default:
+						self?.errorLabel.text = "There was a problem"
+					}
+					self?.errorLabel.isHidden = false
+					self?.tedtalks = []
+				}
+			}
+		}
+
+		tedTalksTableView.dataSource = self
+		tedTalksTableView.delegate = self
+		searchField.delegate = self
+	}
 }
 
 extension TedTalksViewController: UITableViewDataSource {
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tedtalks?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellReuseIdentifier = "tedTalkCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
-        cell.textLabel?.text = "\(getYearPublished(indexPath: indexPath)) - \(tedtalks![indexPath.row].name)"
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegue(withIdentifier: "tedTalkSegue", sender: self)
-    }
-    
-    private func getYearPublished(indexPath: IndexPath) -> Int {
-        let teddate = NSDate(timeIntervalSince1970: tedtalks![indexPath.row].publishedDate)
-        let calendar = Calendar.current
-        let year = calendar.component(.year, from: teddate as Date)
-        return year
-    }
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if !(tedtalksFiltered?.isEmpty ?? true) {
+			return tedtalksFiltered?.count ?? 0
+		}
+		return !(searchField.text?.isEmpty ?? true) ? 0 : tedtalks?.count ?? 0
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "tedTalkCell", for: indexPath)
+		if !(tedtalksFiltered?.isEmpty ?? true) {
+			cell.textLabel?.text = "\(getYearPublished(dateTimestamp: tedtalksFiltered?[indexPath.row].publishedDate ?? 0)) - \(tedtalksFiltered![indexPath.row].name)"
+		} else {
+			cell.textLabel?.text = "\(getYearPublished(dateTimestamp: tedtalks?[indexPath.row].publishedDate ?? 0)) - \(tedtalks![indexPath.row].name)"
+		}
+		return cell
+	}
+
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		self.performSegue(withIdentifier: "tedTalkSegue", sender: self)
+	}
+
+	private func getYearPublished(dateTimestamp: Double) -> Int {
+		let teddate = NSDate(timeIntervalSince1970: dateTimestamp)
+		return Calendar.current.component(.year, from: teddate as Date)
+	}
 }
 
 extension TedTalksViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "tedTalkSegue", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (segue.identifier == "tedTalkSegue") {
-            if let destination = segue.destination as? TedTalkDetailViewController {
-                destination.tedtalk = tedtalks?[(tedTalksTableView.indexPathForSelectedRow?.row)!]
-            }
-        }
-    }
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		performSegue(withIdentifier: "tedTalkSegue", sender: self)
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "tedTalkSegue" {
+			if let destination = segue.destination as? TedTalkDetailViewController {
+				destination.tedtalk = tedtalks?[(tedTalksTableView.indexPathForSelectedRow?.row)!]
+			}
+		}
+	}
+}
+
+extension TedTalksViewController {
+
+	func textFieldDidChangeSelection(_ textField: UITextField) {
+		if let searchText = searchField.text {
+			filterData(searchText)
+		}
+	}
+
+	func filterData(_ query: String?) {
+		guard let query = query else {
+			return
+		}
+		tedtalksFiltered?.removeAll()
+		tedtalks?.forEach({ tedtalk in
+			if getYearPublished(dateTimestamp: tedtalk.publishedDate) == Int(query) {
+				tedtalksFiltered?.append(tedtalk)
+			}
+		})
+		self.tedTalksTableView.reloadData()
+	}
 }
--- Ted Talk/Network/TedTalksManager.swift
+++ Ted Talk/Network/TedTalksManager.swift
@@ -8,34 +8,38 @@
 import Foundation
 
 enum Errors: Error {
-    case fileNotFound
-    case invalidData
-    case decodingProblem(String)
+	case fileNotFound
+	case invalidData
+	case decodingProblem(String)
 }
 
-class TedTalksManager {
-    func retrieve(onCompletion: @escaping (Result<[TedTalk], Errors>) -> Void) {
-        
-        DispatchQueue.global(qos: .background).async {
-            let url = Bundle.main.url(forResource: "tedTalks", withExtension: "json")
-            guard let myUrl = url else {
-                onCompletion(.failure(.fileNotFound))
-                return
-            }
-            guard let myData = try? Data(contentsOf: myUrl) else {
-                onCompletion(.failure(.invalidData))
-                return
-            }
-            do {
-                let tedtalks = try JSONDecoder().decode([TedTalk].self, from: myData)
-                onCompletion(.success(tedtalks))
-            } catch DecodingError.dataCorrupted(_) {
-                onCompletion(.failure(.decodingProblem("Data corrupted")))
-            } catch DecodingError.keyNotFound(let codingKey, _) {
-                onCompletion(.failure(.decodingProblem(codingKey.stringValue)))
-            } catch let error {
-                onCompletion(.failure(.decodingProblem(error.localizedDescription)))
-            }
-        }
-    }
+protocol TedTalksManagerProtocol {
+	func retrieve(onCompletion: @escaping (Result<[TedTalk], Errors>) -> Void)
+}
+
+class TedTalksManager: TedTalksManagerProtocol {
+	func retrieve(onCompletion: @escaping (Result<[TedTalk], Errors>) -> Void) {
+
+		DispatchQueue.global(qos: .background).async {
+			let url = Bundle.main.url(forResource: "tedTalks", withExtension: "json")
+			guard let myUrl = url else {
+				onCompletion(.failure(.fileNotFound))
+				return
+			}
+			guard let myData = try? Data(contentsOf: myUrl) else {
+				onCompletion(.failure(.invalidData))
+				return
+			}
+			do {
+				let tedtalks = try JSONDecoder().decode([TedTalk].self, from: myData)
+				onCompletion(.success(tedtalks))
+			} catch DecodingError.dataCorrupted(_) {
+				onCompletion(.failure(.decodingProblem("Data corrupted")))
+			} catch DecodingError.keyNotFound(let codingKey, _) {
+				onCompletion(.failure(.decodingProblem(codingKey.stringValue)))
+			} catch let error {
+				onCompletion(.failure(.decodingProblem(error.localizedDescription)))
+			}
+		}
+	}
 }
